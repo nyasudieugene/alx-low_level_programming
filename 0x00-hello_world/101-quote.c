c0a007ceff08a7f7d47264b79b405287fdb76f36
nyasudieugene <nyasudieugene@gmail.com>
2023-06-19 09:34:38 -0700

    Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <string.h>
+#include <sys/syscall.h>
+
+int main(void) {
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    syscall(SYS_write, STDERR_FILENO, message, strlen(message));
+    return 1;
+}
